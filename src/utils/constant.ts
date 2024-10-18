d18240ed2e784bff75b17c2a67ab02baed39a83d
Khoang Vo <vodangkhoangute@gmail.com>
2024-10-18 14:42:58 +0700

    feat: protect data in constant file

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -8,13 +8,13 @@ export enum Modes {
   SHORT_BREAK = 'Short break',
   LONG_BREAK = 'Long break',
 }
-export const MESSAGE_MODES = {
+export const MESSAGE_MODES = Object.freeze({
   [Modes.FOCUS]: "Time's up! Take a break.",
   [Modes.SHORT_BREAK]: 'Short break is over. Back to work!',
   [Modes.LONG_BREAK]: 'Long break is over. Ready to focus again?',
-};
+});
 
-export const NON_EXISTING_PROJECT = {
+export const NON_EXISTING_PROJECT = Object.freeze({
   label: 'None',
   id: null as NonExistingProjectId,
-};
+});
